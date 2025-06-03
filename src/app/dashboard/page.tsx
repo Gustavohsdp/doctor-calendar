7e863a4ab5130889d6ea6b633fb16bfab66ba2fe
Gustavo Henrique <gustavohsdp@gmail.com>
2025-06-02 21:39:58 -0300

    feat: implement authentication checks in pages and add sign-out button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,22 @@
-export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+export default async function Dashboard() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authetication");
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+    </div>
+  );
 }
