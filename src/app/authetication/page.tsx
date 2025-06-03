96591245c07e64657dbd8b404c154a6a972de813
Gustavo Henrique <gustavohsdp@gmail.com>
2025-06-02 22:52:54 -0300

    feat: add clinic form page

--- src/app/authetication/page.tsx
+++ src/app/authetication/page.tsx
@@ -12,7 +12,7 @@ export default async function AutheticationPage() {
     headers: await headers(),
   });
 
-  if (!session?.user) {
+  if (session?.user) {
     redirect("/dashboard");
   }
 
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
+
+import { SignOutButton } from "./components/sign-out-button";
 
 export default async function Dashboard() {
   const session = await auth.api.getSession({
@@ -12,11 +17,21 @@ export default async function Dashboard() {
     redirect("/authetication");
   }
 
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
       <h1>{session?.user?.name}</h1>
       <h1>{session?.user?.email}</h1>
+
+      <SignOutButton />
     </div>
   );
 }
